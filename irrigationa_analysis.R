1f50c7ecd4de34c544773417da06cc1ed6f40d49
Bishal Nneupane <66203274+bishalnneupane1@users.noreply.github.com>
2023-10-21 13:28:55 +0545

    2 factor rcbd completed

---
+++ irrigationa_analysis.R
@@ -0,0 +1,20 @@
+library(readxl)
+Book <- read_excel("C:/Users/bisha/OneDrive - bishalnneupane/Desktop/r analysis/irrigation.xlsx")
+
+Book$fertilizer = as.factor(Book$fertilizer)
+Book$irrigation = as.factor(Book$irrigation)
+Book$block = as.factor(Book$block)
+
+require(gvlma)
+require(agricolae)
+
+bishal = gvlma.lm(yield~block+fertilizer*irrigation, data=Book)
+bishal
+
+binay = anova(bishal)
+binay
+
+edf = binay$Df[5]
+ems = binay$`Mean Sq`[5]
+
+lsd = with(Book, LSD.test())
